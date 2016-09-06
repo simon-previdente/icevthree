abcf8442422c084f9c8d2f3648093855f89e507e
Simon PREVIDENTE <simon.previdente@free.fr>
2016-09-06 17:55:12 +0200

    Add other tests in exponentiation test class

--- core/src/test/java/core/ExponentiationTest.java
+++ core/src/test/java/core/ExponentiationTest.java
@@ -11,4 +11,18 @@ public class ExponentiationTest {
         assertEquals(1, ExponentiationMachine.getInstance().calculate(1, 1), 0);
     }
 
+    @Test
+    public void testZeroExponentialZeroShouldReturnOne() {
+        assertEquals(1, ExponentiationMachine.getInstance().calculate(0, 0), 0);
+    }
+
+    @Test
+    public void testOneExponentialMinusOneShouldReturnOne() {
+        assertEquals(1, ExponentiationMachine.getInstance().calculate(1, -1), 0);
+    }
+
+    @Test
+    public void testTwoExponentialThreeShouldReturnEight() {
+        assertEquals(8, ExponentiationMachine.getInstance().calculate(2, 3), 0);
+    }
 }
